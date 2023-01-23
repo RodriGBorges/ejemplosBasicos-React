f1d78bce9a2faa661c2824d2bbe39a944b5fd015
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-01-23 17:49:26 -0300

    Solucion de bugs en el cronometro

--- src/components/StopwatchTimer/index.jsx
+++ src/components/StopwatchTimer/index.jsx
@@ -1,74 +1,59 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Button, ButtonGroup, Card, Col, Container, Row } from 'react-bootstrap';
 
 const oneOrTwoNum = (num) => num > 9 ? num : `0${num}`;
+const oneSecondToMilisecond = 1000;
 
 export const StopwatchTimer = () => {
     
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [hours, setHours] = useState(0);
+    const timerInitialState = {seconds: 0, minutes: 0, hours: 0};
+    const [timer, setTimer] = useState(timerInitialState);
 
     const [velocity, setVelocity] = useState(0);
     const [velocityName, setVelocityName] = useState("");
+    const [intervalState, setIntervalState] = useState(null);
 
-    const [stateIntervalSeconds, setIntervalSeconds] = useState(null);
-    const [stateIntervalMinutes, setIntervalMinutes] = useState(null);
-    const [stateIntervalHours, setIntervalHours] = useState(null);
-    
     const handleStart = () => {
 
+        /* Si existe un intervalo activo que solo lo detenga */
+        if(intervalState) {
+            handleStop();
+        }
 
-        const intervalHours = setInterval(() => {
-            setHours((hrs) => {
-                return hrs + 1;
-            });
-        }, velocity ? (61000 * 60) / velocity : (61000 * 60));
-        
-        const intervalMinutes = setInterval(() => {
-            setMinutes((min) => {
-                if(min === 59) {
-                    return 0;
+        const interval = setInterval(() => {
+            setTimer((t) => {
+                if(t.seconds === 59) {
+                    return {...t, seconds: 0, minutes: t.minutes + 1};
                 }
-                return min + 1;
-            });
-        }, velocity ? 61000 / velocity : 61000);
-        
-        const intervalSeconds = setInterval(() => {
-            setSeconds((seg) => {
-                if(seg === 59) {
-                    return 0;
+                if(t.minutes === 59) {
+                    return {...t, minutes: 0, hours: t.hours + 1};
                 }
-                return seg + 1;
+                return {...t, seconds: t.seconds + 1};
             });
-        }, velocity ? 1000 / velocity : 1000);
+        }, velocity ? oneSecondToMilisecond / velocity : oneSecondToMilisecond);
 
-        setIntervalSeconds(intervalSeconds);
-        setIntervalMinutes(intervalMinutes);
-        setIntervalHours(intervalHours);
+        setIntervalState(interval);
 
     };
 
     const handleStop = () => {
-        if(stateIntervalSeconds && stateIntervalMinutes && stateIntervalHours) {
-            clearInterval(stateIntervalSeconds);
-            clearInterval(stateIntervalMinutes);
-            clearInterval(stateIntervalHours);
+        if(!intervalState) {
+            console.log('No hay intervalo activo');
+            return
         }
 
-        setIntervalSeconds(null);
-        setIntervalMinutes(null);
-        setIntervalHours(null);
+        clearInterval(intervalState);
     };
 
     const handleReset = () => {
         handleStop();
-        setSeconds(0);
-        setMinutes(0);
-        setHours(0);
+        setTimer(timerInitialState);
     };
 
     const handleVelocity = (vel = null, velText) => {
+        if(intervalState) {
+            handleStop();
+        }
         setVelocity(vel);
         handleStop();
         setVelocityName(velText);
@@ -79,25 +64,25 @@ export const StopwatchTimer = () => {
             <Row className="mt-5">
                 <Col xs={12} lg={{span: 6, offset: 3}} className="text-center">
                 <ButtonGroup aria-label="Basic example" className="d-block my-1">
-                        <Button variant="outline-success" onClick={handleStart} disabled={stateIntervalSeconds && stateIntervalMinutes && stateIntervalHours}>Comenzar</Button>
+                        <Button variant="outline-success" onClick={handleStart}>Comenzar</Button>
                         <Button variant="outline-danger" onClick={handleStop}>Detener</Button>
                         <Button variant="outline-dark" onClick={handleReset}>Reiniciar</Button>
                 </ButtonGroup>
                 <ButtonGroup aria-label="Basic example">
                         <Button variant="outline-dark" className={velocityName === "min" && "active"} onClick={() => handleVelocity(0, "min")} >Min</Button>
-                        <Button variant="outline-dark" className={velocityName === "x2" && "active"} onClick={() => handleVelocity(2, "x2")} >x2</Button>
-                        <Button variant="outline-dark" className={velocityName === "x4" && "active"} onClick={() => handleVelocity(4, "x4")} >x4</Button>
-                        <Button variant="outline-dark" className={velocityName === "x6" && "active"} onClick={() => handleVelocity(6, "x6")} >x6</Button>
-                        <Button variant="outline-dark" className={velocityName === "max" && "active"} onClick={() => handleVelocity(10, "max")} >Max</Button>
+                        <Button variant="outline-dark" className={velocityName === "x2" && "active"} onClick={() => handleVelocity(10, "x2")} >x2</Button>
+                        <Button variant="outline-dark" className={velocityName === "x4" && "active"} onClick={() => handleVelocity(40, "x4")} >x4</Button>
+                        <Button variant="outline-dark" className={velocityName === "x6" && "active"} onClick={() => handleVelocity(80, "x6")} >x6</Button>
+                        <Button variant="outline-dark" className={velocityName === "max" && "active"} onClick={() => handleVelocity(1000, "max")} >Max</Button>
                 </ButtonGroup>
                 <Card style={{ width: '25rem' }} className="m-auto my-3">
                 <Card.Body>
                     <Card.Title>Stopwatch / Timer</Card.Title>
                     <Card.Text>
-                    {oneOrTwoNum(hours)} horas - {oneOrTwoNum(minutes)} minutos - {oneOrTwoNum(seconds)} segundos
+                    {oneOrTwoNum(timer.hours)} horas - {oneOrTwoNum(timer.minutes)} minutos - {oneOrTwoNum(timer.seconds)} segundos
                     </Card.Text>
                 </Card.Body>
-                <Card.Footer>{oneOrTwoNum(hours)}:{oneOrTwoNum(minutes)}:{oneOrTwoNum(seconds)}</Card.Footer>
+                <Card.Footer>{oneOrTwoNum(timer.hours)}:{oneOrTwoNum(timer.minutes)}:{oneOrTwoNum(timer.seconds)}</Card.Footer>
                 </Card>
                 </Col>
             </Row>
