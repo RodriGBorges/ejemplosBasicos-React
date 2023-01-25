41a5d84e20180da8343a700f029c818cbc3fe5d2
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-01-25 17:16:23 -0300

    taskApp - Create, read & update

--- src/components/TaskManager/index.jsx
+++ src/components/TaskManager/index.jsx
@@ -1,38 +1,90 @@
-import { useRef } from "react";
+import { useReducer, useRef, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useForm } from "../../hook/useForm";
+import { CardItem } from "./CardItem";
 import { FormTask } from "./Form";
 
+
+const generateId = () => Math.random().toString(36).substring(2, 18);
+
+console.log(generateId());
+
+const taskReducer = (state, action) => { // state = representa el array con todas las tareas
+    
+    switch (action.type) {
+        case "ADD":
+
+            const newTask = {
+                ...action.payload,
+                id: generateId(),
+                active: false,
+                completed: false
+            };
+
+            console.log("Acción Agregar", newTask);
+            return [...state, newTask];
+        
+        case "UPDATE":
+            const taskToUpdate = action.payload
+            const tasksUpdated = state.map((task) => {
+                if(task.id === taskToUpdate.id) {
+                    return {
+                        ...task,
+                        ...taskToUpdate
+                    }
+                }
+                return task;
+            });
+            return tasksUpdated;
+    
+        default:
+            return state;
+    }
+
+}
 
 export const TaskManager = () => {
 
     const refForm = useRef(null);
     const [inputsValues, setInputsValues, handleChangeInputsValue, reset] = useForm({}, refForm);
 
+    const [action, setAction] = useState("CREATE"); //Acción del submit para usar el mismo formulario para actualizar o editar
 
-    /* const [inputsValues, setInputsValues] = useState({});
-
-    const handleChangeInputsValue = ({target: {name, value}}) => {
-        //console.log({ [target.name]:target.value});
-        setInputsValues({
-            ...inputsValues,
-            [name]: value
-        });
-    } */
+    const [tasks, dispatch] = useReducer(taskReducer, []); // dispatch({type, payload})
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if(action === "CREATE") {
+            dispatch({type:"ADD", payload: inputsValues});
+        }
+        if(action === "UPDATE") {
+            dispatch({type:"UPDATE", payload: inputsValues});
+        }
         reset();
+    }
+
+    const handleUpdate = (id) => {
+        console.log('Actualizar la tarea ID: ' + id);
+
+        const taskFound = tasks.find(task => task.id === id);
+        setAction("UPDATE");
+
+        setInputsValues(taskFound);
     }
 
     return (
         <Container className="mt-5">
             <Row>
                 <Col sm={12} lg={3}>
-                    <FormTask onChange={handleChangeInputsValue} inputsValues={inputsValues} onSubmit={handleSubmit} refForm={refForm} />
+                    <FormTask onChange={handleChangeInputsValue} inputsValues={inputsValues} onSubmit={handleSubmit} refForm={refForm} action={action} />
                 </Col>
                 <Col sm={12} lg={9}>
-                    Contenido
+                    {
+                        tasks.map(task => {
+                            return <CardItem key={task.id} task={task} onUpdate={handleUpdate} />
+                        })
+                    }
                 </Col>
             </Row>
         </Container>
